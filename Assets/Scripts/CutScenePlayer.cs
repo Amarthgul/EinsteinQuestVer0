7b11e1c108501532c9c812c5866ff17c0127dc01
Joshua Varghese <joshvarg.sec@gmail.com>
2023-04-13 15:15:37 -0400

    fix: rework for individual slide time control
    Slides are now "instantiated" within Globals. Using a tuple, you enter
    the path to the image and the seconds you want to display the slide for.
    FADE_TIME constant added to Globals for quick modification.
    
    TODO:
    - audio syncing with slides (possibly just extend the tuple structure)

--- Assets/Scripts/CutScenePlayer.cs
+++ Assets/Scripts/CutScenePlayer.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting.ReorderableList.Internal;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,13 +8,10 @@ namespace EinsteinQuest
     public class CutScenePlayer : MonoBehaviour
     {
         [SerializeField] RawImage cutscene;
-        [SerializeField] float freezeTime = 4f;
-        [SerializeField] float fadeTime = 2f;
-        private Object[] scenes;
+        private float fadeTime = Globals.FADE_TIME;
         // Start is called before the first frame update
         void Start()
         {
-            scenes = Resources.LoadAll("Cutscene/TestImages", typeof(Texture2D));
             cutscene.canvasRenderer.SetAlpha(0f);
             StartCoroutine(Play(0));
 
@@ -28,16 +22,17 @@ namespace EinsteinQuest
         {
         }
 
-        IEnumerator Play(int slidenum)
+        IEnumerator Play(int idx)
         {
-            cutscene.texture = (Texture2D)scenes[slidenum];
+            var current = Globals.Slides[idx];
+            cutscene.texture = (Texture2D)Resources.Load(current.path);
             cutscene.CrossFadeAlpha(1, fadeTime, false);
-            yield return new WaitForSeconds(freezeTime);
+            yield return new WaitForSeconds(current.time);
             cutscene.CrossFadeAlpha(0, fadeTime, false);
             yield return new WaitForSeconds(fadeTime);
-            if (slidenum < scenes.Length - 1)
+            if (idx < Globals.Slides.Length - 1)
             {
-                StartCoroutine(Play(slidenum + 1));
+                StartCoroutine(Play(idx+1));
             } else
             {
                 cutscene.canvas.gameObject.SetActive(false);
